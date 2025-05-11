066c2ce58d04358d60a807f5a4947b7b261e2c05
V3-Dump_Bot <girikhon2006@gmail.com>
2025-05-11 16:23:28 +0530

    Update common.php

--- common.php
+++ common.php
@@ -1,5 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  exit(0);
+}
     include_once 'constants.php';
     include_once 'configuration.php';
 
